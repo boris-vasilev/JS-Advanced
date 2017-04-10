1bec351406e22893f40deaaf6be1e61bdc9d3061
Boris <borisvasilev395@gmail.com>
2017-04-10 22:47:15 +0300

    Add files via upload

--- 2.Functions/5.VectorMath.js
+++ 2.Functions/5.VectorMath.js
@@ -4,7 +4,7 @@ function Vector(x,y){
     this.x=x;
     this.y=y;
     this.add=function(v2){
-        if(!v2 instanceof Vector){
+        if(!(v2 instanceof Vector)){
             throw new Error('Input must be an instance of Vector');
         }
         return new Vector(v2.x+this.x,v2.y+this.y);
@@ -16,13 +16,13 @@ function Vector(x,y){
         return Math.sqrt(Math.pow(this.x,2)+Math.pow(this.y,2));
     }
     this.dot = function(v2){
-        if(!v2 instanceof Vector){
+        if(!(v2 instanceof Vector)){
             throw new Error('Input must be an instance of Vector');
         }
         return v2.x*this.x+v2.y*this.y;
     }
     this.cross=function(v2){
-        if(!v2 instanceof Vector){
+        if(!(v2 instanceof Vector)){
             throw new Error('Input must be an instance of Vector');
         }
         return v2.y*this.x-v2.x*this.y;
@@ -32,5 +32,5 @@ Vector.prototype.toString=function(){
     return 'x = '+this.x+'\n'+'y = '+this.y;
 }
 var v1 = new Vector(1,0);
-console.log(v1.dot(new Vector(0,-1)));
+console.log(v1.dot('new Vector(0,-1)'));
 console.log(v1.add(new Vector(1,1)).toString());
